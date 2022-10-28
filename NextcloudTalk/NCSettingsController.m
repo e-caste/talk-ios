af5e8b4691a822aa7858b37115730c0ec21beecd
e-caste-mbp <git@caste.dev>
2022-10-28 19:14:59 +0200

    [IGNORE] changes for local development

--- NextcloudTalk/NCSettingsController.m
+++ NextcloudTalk/NCSettingsController.m
@@ -149,7 +149,7 @@ NSString * const kDidReceiveCallsFromOldAccount = @"receivedCallsFromOldAccount"
         [self subscribeForPushNotificationsForAccountId:accountId withCompletionBlock:nil];
     } else {
         [self setActiveAccountWithAccountId:accountId];
-        [JDStatusBarNotification showWithStatus:@"Account already added" dismissAfter:4.0f styleName:JDStatusBarStyleSuccess];
+//        [JDStatusBarNotification showWithStatus:@"Account already added" dismissAfter:4.0f styleName:JDStatusBarStyleSuccess];
     }
 }
 
